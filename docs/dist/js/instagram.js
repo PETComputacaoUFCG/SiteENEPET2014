75e764e0d4151b256b0337df9c794058a1f937b9
Thiago Almeida <thiago2010.2@gmail.com>
2014-03-17 23:33:47 -0300

    não sei o que dizer dessas alterações :))

--- docs/dist/js/instagram.js
+++ docs/dist/js/instagram.js
@@ -1,7 +1,6 @@
 var feed = new Instafeed({
             	get: 'tagged',
-           		tagName: 'ENEPET2013',
-           		limit:7,
+           		tagName: 'ENEPET2014',
             	clientId: 'e94d88ecc3314049aabdbbf168a950cb'
             });
             feed.run();
